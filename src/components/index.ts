65dcec8cff5a48feca03256bb9618630b7adebbf
Nans Laupretre <nans.laupretre@gmail.com>
2022-06-11 19:34:12 +0200

    Update import after rename

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 // controls
-export { default as Button } from './controls/button/Button'
+export { default as Button } from './controls/Button/Button'
 export { default as CheckBox } from './controls/CheckBox/CheckBox'
 export { default as TextInput } from './controls/TextInput/TextInput'
 export { default as SelectInput } from './controls/SelectInput/SelectInput'
@@ -8,16 +8,16 @@ export { default as DatePicker } from './controls/DatePicker/DatePicker'
 export { default as SwitchInput } from './controls/Switch/SwitchInput'
 
 // ui
-export { default as Toaster } from './ui/toaster/Toaster'
-export { default as Main } from './ui/main/Main'
-export { default as NavBar } from './ui/navbar/Navbar'
+export { default as Toaster } from './ui/Toaster/Toaster'
+export { default as Main } from './ui/Main/Main'
+export { default as NavBar } from './ui/Navbar/Navbar'
 export { default as SectionTitle } from './ui/SectionTitle/SectionTitle'
-export { default as Dialog } from './ui/dialog/Dialog'
-export { toastSender } from './ui/toaster/ToastSender'
+export { default as Dialog } from './ui/Dialog/Dialog'
+export { toastSender } from './ui/Toaster/ToastSender'
 
 // functional
 export { default as AuthProvider } from './functional/AuthProvider/AuthProvider'
 export { default as AuthForm } from './functional/AuthForm/AuthForm'
-export { default as AccountTile } from './functional/account/AccountTile/AccountTile'
-export { default as AccountTileNew } from './functional/account/AccountTileNew/AccountTileNew'
+export { default as AccountTile } from './functional/Account/AccountTile/AccountTile'
+export { default as AccountTileNew } from './functional/Account/AccountTileNew/AccountTileNew'
 export { default as OperationsTable } from './functional/OperationsTable/OperationsTable'
