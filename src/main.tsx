d8b551af0479171623a9f4f44de2e7bdb922cec1
Nans Laupretre <nans.laupretre@gmail.com>
2023-07-17 09:16:53 +0200

    chore - api mocking update

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 
 import '@/lib/react-i18next'
 import App from './App.tsx'
-import { initMocks } from './test/server'
+import { API_MOCKING } from './config/index.ts'
 
-initMocks()
+if (API_MOCKING) {
+  import('./test/server').then(({ initMocks }) => initMocks())
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
